d6307e35fb2311f1844da2cf87fdd617b032f618
benjamindam <bdamsg19@student.aau.dk>
2019-11-06 16:08:20 +0100

    midlertidige ændringer

--- battery_check/src/battery.cpp
+++ battery_check/src/battery.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <actionlib>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello" << endl;
-}
+// Create the client
+  actionlib::SimpleActionClient<kobuki_msgs::AutoDockingAction> docking_ac("dock_drive_action", true);
+  docking_ac.waitForServer();
+
+  // Create goal object
+  kobuki_msgs::AutoDockingGoal goal;
+
+  // Assign initial state
+  actionlib::SimpleClientGoalState dock_state = actionlib::SimpleClientGoalState::LOST;
+
+  // Send the goal
+  docking_ac.sendGoal(goal);
+
+  ros::Time time = ros::Time::now();
+
+  // Monitor progress
+  while (!docking_ac.waitForResult(ros::Duration(3))) {
+
+    dock_state = docking_ac.getState();
+    ROS_INFO("Docking status: %s",dock_state.toString().c_str());
+
+    if (ros::Time::now() > (time+ros::Duration(10))) {
+      ROS_INFO("Docking took more than 10 seconds, canceling.");
+      docking_ac.cancelGoal();
+      break;
+    }// end if
+  }// end while
