399c57b87f056036ee0f343631b8e3cff09c7981
benjamindam <bdamsg19@student.aau.dk>
2019-11-15 15:54:39 +0100

    Batteritest der virker

--- battery_check/src/battery.cpp
+++ battery_check/src/battery.cpp
@@ -1,32 +1,40 @@
 #include <iostream>
-#include <actionlib>
+#include <ros/ros.h>
+#include <kobuki_msgs/SensorState.h>
+#include <std_msgs/String.h>
 
-using namespace std;
 
-// Create the client
-  actionlib::SimpleActionClient<kobuki_msgs::AutoDockingAction> docking_ac("dock_drive_action", true);
-  docking_ac.waitForServer();
+ void batteryCallback(const kobuki_msgs::SensorState& msg)
+  {
+    float bat = msg.battery/10.00;        //batteriets spænding i V
+    int ska = 100.0/3.3;                  //skalar for %-udregningen
+    float ver = bat-13.0;                 //værdi for batteriets kapacitet hvis 13v er 0%
+    float res = ska*ver;                  //resultatet for udregningen
+    //std::cout << "Batteri: " << bat << " volt" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Batteri: " << res << "%" << std::endl;
+    std::cout << "-----------------------------------------------" << std::endl;
 
-  // Create goal object
-  kobuki_msgs::AutoDockingGoal goal;
+    if (res < 30)                         //hvis batteriprocenten er under 30%
+    {
+      std::cout << "STIK MIG EN ØL" << std::endl;
+    }
+  }
+ 
+ int main(int argc, char **argv)
+  {
+  
+    ros::init(argc, argv, "battery");
+ 
+  
+    ros::NodeHandle n;
+ 
+   
+    ros::Subscriber sub = n.subscribe("/mobile_base/sensors/core", 1000, batteryCallback);
 
-  // Assign initial state
-  actionlib::SimpleClientGoalState dock_state = actionlib::SimpleClientGoalState::LOST;
+  
+    ros::spin();
 
-  // Send the goal
-  docking_ac.sendGoal(goal);
+     return 0;
+  }
 
-  ros::Time time = ros::Time::now();
-
-  // Monitor progress
-  while (!docking_ac.waitForResult(ros::Duration(3))) {
-
-    dock_state = docking_ac.getState();
-    ROS_INFO("Docking status: %s",dock_state.toString().c_str());
-
-    if (ros::Time::now() > (time+ros::Duration(10))) {
-      ROS_INFO("Docking took more than 10 seconds, canceling.");
-      docking_ac.cancelGoal();
-      break;
-    }// end if
-  }// end while
