07fe2af5f3660baed27fdad73ac2e6eb972a55f1
benjamindam <bdamsg19@student.aau.dk>
2019-11-18 16:02:25 +0100

    rettelser

--- rooms/src/room1.cpp
+++ rooms/src/room1.cpp
@@ -4,11 +4,8 @@
 
 geometry_msgs::Twist drive(geometry_msgs::Twist &msg)
 {
-        msg.angular.z = 1;
-    
-    
-    std::cout << "Jeg drejer rundt med: " << msg.angular.z << " km/t" << std::endl;
-    std::cout << "Undskyld, men hvor er rum 1 henne?" << std::endl;
+    msg.angular.z = 1;
+    std::cout << "Jeg er på vej til rum 1 med, " << msg.angular.z << " km/t." << std::endl;
     return msg;
 }
 
@@ -17,23 +14,17 @@ geometry_msgs::Twist drive(geometry_msgs::Twist &msg)
 int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "room1");
-    srand(time(NULL));
     ros::NodeHandle n;
-    ros::Rate loop_rate(5);
-    
+    ros::Rate loop_rate(10);
     
     ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1, drive);
     geometry_msgs::Twist msg;
+
     while(ros::ok())
     {
-    
     cmd_vel_pub.publish(drive(msg));
     loop_rate.sleep();
-    
-
     }
  
-    
-    
     return 0;
 }
