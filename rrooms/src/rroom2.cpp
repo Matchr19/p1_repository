4317c8a5f4c35931797cf2b7e2b97625756ae454
benjamindam <bdamsg19@student.aau.dk>
2019-11-19 10:30:11 +0100

    rum er done

---
+++ rrooms/src/rroom2.cpp
@@ -0,0 +1,32 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <math.h>
+
+int main(int argc, char **argv)
+{
+const double PI = 3.14159265358979323846;
+
+ros::init(argc, argv, "rroom2");
+ros::NodeHandle n;
+ros::Publisher movement_pub = n.advertise<geometry_msgs::Twist>("mobile_base/commands/velocity",1); //for sensors the value after , should be higher to get a more accurate result (queued)
+ros::Rate rate(10); //the larger the value, the "smoother" , try value of 1 to see "jerk" movement
+
+std::cout << "Kører hjem fra rum 2" << std::endl;
+
+//turn right
+ros::Time start_turn = ros::Time::now();
+while(ros::Time::now() - start_turn < ros::Duration(20.0))
+{
+    geometry_msgs::Twist move;
+    //velocity controls
+    move.linear.x = 0; //speed value m/s
+    move.angular.z = 3;
+    movement_pub.publish(move);
+
+    ros::spinOnce();
+    rate.sleep();
+}
+
+
+return 0;
+}
