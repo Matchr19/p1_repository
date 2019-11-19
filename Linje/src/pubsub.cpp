461fc2fccb0ab6723e0fa34cfebcc88b9529fcaa
Jacob Knudsen <jknuds19@student.aau.dk>
2019-11-19 13:34:22 +0100

    Jacob update

--- Linje/src/pubsub.cpp
+++ Linje/src/pubsub.cpp
@@ -7,35 +7,53 @@
 geometry_msgs::Twist msg;
 kobuki_msgs::BumperEvent bumpMsg;
 ros::Publisher cmd_vel_pub;
+void sendPub(double ligeud, double drej)
+{
+    msg.linear.x = ligeud;
+    msg.angular.z = drej;
+    std::cout << "Linear: " 
+    << msg.linear.x << " "
+    << "Angular: " << msg.angular.z << std::endl;
+    cmd_vel_pub.publish(msg);
+}
 
 void drive()
 {
-    int drivenumb = rand() % 3;
-    if (drivenumb == 0)
-    {
-        msg.linear.x = 0.2;
-        msg.angular.z = 0;
-    }
-    else if (drivenumb == 1)
-    {
-        msg.linear.x = 0;
-        msg.angular.z = 0.5;
-    }
-    else if (drivenumb == 2)
-    {
-        msg.linear.x = 0;
-        msg.angular.z = -0.5;
-    }
+    std::string Retning {""};
+    std::cout << "\n\n\n\n\n\n\n\nWhere should i go?" << std::endl;
+    std::cout << "You have the following options: " << std::endl;
+    std::cout << "Left" << std::endl << "Straight" << std::endl << "Right\n\n\n\n\n" << std::endl << "Input: ";
+    std::cin >> Retning;
+    
+    std::cout << "Going " << Retning << std::endl;
 
-    std::cout << "Linear: " << msg.linear.x << " "
-              << "Angular: " << msg.angular.z << std::endl;
-      cmd_vel_pub.publish(msg);      
+
+    double x {};
+    double y {};
+    if (Retning == "Straight")
+    {
+        x = 0.2;
+        y = 0.;
+    }
+    else if (Retning == "Left")
+    {
+        x = 0.1;
+        y = 0.5;
+    }
+    else if (Retning == "Right")
+    {
+        x = 0.1;  
+        y = -0.5;
+    }
+    
+    
+    sendPub(x, y);   
+        
 
 }
 
 void bump(const kobuki_msgs::BumperEvent &bumpMsg)
 {
-
     if (bumpMsg.state == 1)
     {
         int bumpo = bumpMsg.bumper;
@@ -52,18 +70,26 @@ void bump(const kobuki_msgs::BumperEvent &bumpMsg)
         {
             bumpertekst = " Right bumper";
         }
-        msg.linear.x = 0;
-        msg.angular.z = 10;
-        cmd_vel_pub.publish(msg);
-
-
-
-
-
-        std::cout << "JEG ER KØRT IND I NOGET med:" << bumpertekst << std::endl;
         
+        sendPub(0, 5);
+        ros::spinOnce();
+        
+
+
+
+
+
+        std::cout << "\n\n\n\n\n\nWOOOOOPS you drove me into something with: " << bumpertekst << std::endl
+        << "Turning around\n\n";
+        
+    } 
+    else
+    {
+        drive();
+        ros::spinOnce();
     }
 }
+
 
 int main(int argc, char *argv[])
 {
@@ -71,9 +97,13 @@ int main(int argc, char *argv[])
     srand(time(NULL));
     ros::NodeHandle n;
     ros::Rate loop_rate(0.4);
-    ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 5);
+    cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1);
     ros::Subscriber bumperSub = n.subscribe("/mobile_base/events/bumper", 1, bump);
-    
-    
+    while(ros::ok())
+    {
+    bump(bumpMsg);
+    ros::spinOnce();
+    loop_rate.sleep();
+    }
     return 0;
 }
