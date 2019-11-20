79fad25ec32d45d8be918db1f6aae8d83fadbccf
Jacob Knudsen <jknuds19@student.aau.dk>
2019-11-20 14:49:26 +0100

    Jacob Vacuum programmer

--- Linje/src/vacuum.cpp
+++ Linje/src/vacuum.cpp
@@ -4,82 +4,71 @@
 #include <iostream>
 #include <string>
 
-geometry_msgs::Twist msg;
 kobuki_msgs::BumperEvent bumpMsg;
 ros::Publisher cmd_vel_pub;
 void sendPub(double ligeud, double drej)
 {
-    msg.linear.x = ligeud;
-    msg.angular.z = drej;
-    std::cout << "Linear: " 
-    << msg.linear.x << " "
-    << "Angular: " << msg.angular.z << std::endl;
-    cmd_vel_pub.publish(msg);
-    ros::spinOnce();
 }
 
 void drive()
 {
-
-    double x {};
-    double y {};
+    geometry_msgs::Twist msg;
+    double x{};
+    double y{};
     int drivenumb = rand() % 3;
     if (drivenumb == 0)
     {
-        x = 0.5;
+        x = 0.15;
         y = 0;
     }
     else if (drivenumb == 1)
     {
-        x = 0.2;
-        y = 0.5;
+        x = 0.1;
+        y = 0.3;
     }
     else if (drivenumb == 2)
     {
-        x = 0.2;  
-        y = -0.5;
+        x = 0.1;
+        y = -0.3;
     }
-    
-    sendPub(x, y);   
-        
-
+    msg.linear.x = x;
+    msg.angular.z = y;
+    std::cout << "Linear: "
+              << msg.linear.x << " "
+              << "Angular: " << msg.angular.z << std::endl;
+    cmd_vel_pub.publish(msg);
 }
 
 void bump(const kobuki_msgs::BumperEvent &bumpMsg)
 {
     if (bumpMsg.state == 1)
     {
+        geometry_msgs::Twist msg;
         int bumpo = bumpMsg.bumper;
         std::string bumpertekst("");
-       
-        
-        sendPub(0, 10);
         std::cout << "JEG ER KØRT IND I NOGET med:" << std::endl;
-        ros::spinOnce();
-        
-    } 
-    else
-    {
-        drive();
-        
+        msg.linear.x = 0.0;
+        msg.angular.z = 10.0;
+        std::cout << "Linear: "
+                  << msg.linear.x << " "
+                  << "Angular: " << msg.angular.z << std::endl;
+        cmd_vel_pub.publish(msg);
+        ros::Duration(1.0).sleep();
     }
 }
 
-
 int main(int argc, char *argv[])
-{ 
+{
     ros::init(argc, argv, "pubsub");
     srand(time(NULL));
     ros::NodeHandle n;
-    ros::Rate loop_rate(0.4);
     cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1);
-    ros::Subscriber bumperSub = n.subscribe("/mobile_base/events/bumper", 2, bump);
-    while(ros::ok())
+    ros::Subscriber bumperSub = n.subscribe("/mobile_base/events/bumper", 1, bump);
+    while (ros::ok())
     {
-    std::cout << "Jeg er kommet ind i loopet" << std::endl;
-    bump(bumpMsg);
-    loop_rate.sleep();
-    ros::spinOnce();
+        drive();
+        ros::Duration(0.5).sleep();
+        ros::spinOnce();
     }
     return 0;
 }
