4741119a8a89f06e819c1338872b77ba25253fa9
benjamindam <bdamsg19@student.aau.dk>
2019-11-21 11:23:49 +0100

    rooms er fucked nu

--- rrooms/src/rroom3.cpp
+++ rrooms/src/rroom3.cpp
@@ -1,32 +1,73 @@
 #include <ros/ros.h>
+#include <tf/tf.h>
 #include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose2D.h>
+#include <nav_msgs/Odometry.h>
 #include <math.h>
+
+geometry_msgs::Pose2D current_pose;
+ros::Publisher pub_pose2d;
+
+void odomCallback(const nav_msgs::OdometryConstPtr& msg)
+{
+    // linear position
+    current_pose.x = msg->pose.pose.position.x;
+    current_pose.y = msg->pose.pose.position.y;
+
+    // quaternion to RPY conversion
+    tf::Quaternion q(
+        msg->pose.pose.orientation.x,
+        msg->pose.pose.orientation.y,
+        msg->pose.pose.orientation.z,
+        msg->pose.pose.orientation.w);
+    tf::Matrix3x3 m(q);
+    double roll, pitch, yaw;
+    m.getRPY(roll, pitch, yaw);
+
+    // angular position
+    current_pose.theta = yaw;
+    pub_pose2d.publish(current_pose);
+}
 
 int main(int argc, char **argv)
 {
-const double PI = 3.14159265358979323846;
+    const double PI = 3.14159265358979323846;
 
-ros::init(argc, argv, "rroom3");
-ros::NodeHandle n;
-ros::Publisher movement_pub = n.advertise<geometry_msgs::Twist>("mobile_base/commands/velocity",1); //for sensors the value after , should be higher to get a more accurate result (queued)
-ros::Rate rate(10); //the larger the value, the "smoother" , try value of 1 to see "jerk" movement
+    ROS_INFO("start");
 
-std::cout << "Kører hjem fra rum 3" << std::endl;
-
-//turn right
-ros::Time start_turn = ros::Time::now();
-while(ros::Time::now() - start_turn < ros::Duration(20.0))
-{
-    geometry_msgs::Twist move;
-    //velocity controls
-    move.linear.x = 0; //speed value m/s
-    move.angular.z = 7;
-    movement_pub.publish(move);
-
-    ros::spinOnce();
-    rate.sleep();
-}
+    ros::init(argc, argv, "move_pub");
+    ros::NodeHandle n;
+    ros::Subscriber sub_odometry = n.subscribe("odom", 1, odomCallback);
+    ros::Publisher movement_pub = n.advertise("cmd_vel",1); //for sensors the value after , should be higher to get a more accurate result (queued)
+    pub_pose2d = n.advertise("turtlebot_pose2d", 1);
+    ros::Rate rate(10); 
 
 
-return 0;
+    //turn right
+    ROS_INFO("turn right");
+    ros::Time start_turn = ros::Time::now();
+    while(ros::ok() && current_pose.theta > -PI/2)
+    {
+        geometry_msgs::Twist move;
+        //velocity controls
+        move.linear.x = 0; //speed value m/s
+        move.angular.z = -0.3;
+        movement_pub.publish(move);
+
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    // just stop
+    while(ros::ok()) {
+        geometry_msgs::Twist move;
+        move.linear.x = 0;
+        move.angular.z = 0;
+        movement_pub.publish(move);
+
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    return 0;
 }
