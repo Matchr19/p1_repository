bf7761affab88f5dbe37baacf1bbc037add5db03
Jacob Knudsen <jknuds19@student.aau.dk>
2019-11-26 12:13:05 +0100

    OPdatering

--- Linje/src/vacuum.cpp
+++ Linje/src/vacuum.cpp
@@ -1,17 +1,21 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <kobuki_msgs/BumperEvent.h>
+#include <kobuki_msgs/Led.h>
 #include <iostream>
 #include <string>
-
+kobuki_msgs::Led LEDmsg;
 kobuki_msgs::BumperEvent bumpMsg;
 ros::Publisher cmd_vel_pub;
-void sendPub(double ligeud, double drej)
-{
-}
+ros::Publisher LED1_pub;
+ros::Publisher LED2_pub;
+
 
 void drive()
 {
+    LEDmsg.value = 1;
+    LED1_pub.publish(LEDmsg);
+    LED2_pub.publish(LEDmsg);
     geometry_msgs::Twist msg;
     double x{};
     double y{};
@@ -43,17 +47,27 @@ void bump(const kobuki_msgs::BumperEvent &bumpMsg)
 {
     if (bumpMsg.state == 1)
     {
+        LEDmsg.value = 3;
+        LED1_pub.publish(LEDmsg);
+        LED2_pub.publish(LEDmsg);
         geometry_msgs::Twist msg;
         int bumpo = bumpMsg.bumper;
         std::string bumpertekst("");
         std::cout << "JEG ER KØRT IND I NOGET med:" << std::endl;
-        msg.linear.x = 0.0;
-        msg.angular.z = 10.0;
-        std::cout << "Linear: "
-                  << msg.linear.x << " "
-                  << "Angular: " << msg.angular.z << std::endl;
-        cmd_vel_pub.publish(msg);
+        for (int i = 0; i < 3; i++)
+        {
+            msg.linear.x = 0.0;
+            msg.angular.z = 2.0;
+            std::cout << "Linear: "
+                      << msg.linear.x << " "
+                      << "Angular: " << msg.angular.z << std::endl;
+            cmd_vel_pub.publish(msg);
+            ros::Duration(0.5).sleep();
+        }
         ros::Duration(1.0).sleep();
+        LEDmsg.value = 1;
+        LED1_pub.publish(LEDmsg);
+        LED2_pub.publish(LEDmsg);
     }
 }
 
@@ -63,6 +77,8 @@ int main(int argc, char *argv[])
     srand(time(NULL));
     ros::NodeHandle n;
     cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1);
+    LED1_pub = n.advertise<kobuki_msgs::Led>("/mobile_base/commands/led1", 1);
+    LED2_pub = n.advertise<kobuki_msgs::Led>("/mobile_base/commands/led2", 1);
     ros::Subscriber bumperSub = n.subscribe("/mobile_base/events/bumper", 1, bump);
     while (ros::ok())
     {
