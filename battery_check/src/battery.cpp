b3b645a24ce50e75b2453d2c16ea8bc384336a9b
benjamindam <bdamsg19@student.aau.dk>
2019-11-28 11:23:30 +0100

    lidt fix

--- battery_check/src/battery.cpp
+++ battery_check/src/battery.cpp
@@ -10,12 +10,10 @@
  void batteryCallback(const kobuki_msgs::SensorState& msg)
   {
     float bat = msg.battery/10.0;          //batteriets spænding i V
-    int ska = 100.0/3.3;                   //skalar for %-udregningen
+    float ska = 100.0/3.3;                 //skalar for %-udregningen
     float ver = bat-13.0;                  //værdi for batteriets kapacitet hvis 13v er 0%
     float res = ska*ver;                   //resultatet for udregningen
-    std::string rum = "lige";              //funktionen der skal subscribe på hvor den skal hen
-    std::cout << std::endl;
-    std::cout << "Batteri: " << res << "%" << std::endl;
+    std::cout << "\nBatteri: " << res << "%" << std::endl;
     std::cout << "-----------------------------------------------" << std::endl;
 
     if (res < 70)                          //hvis batteriprocenten er under 70% så:
@@ -32,16 +30,12 @@
  
  int main(int argc, char **argv)
   {
-  
     ros::init(argc, argv, "battery");
  
-  
     ros::NodeHandle n;
  
-   
     ros::Subscriber sub = n.subscribe("/mobile_base/sensors/core", 10, batteryCallback);
 
-  
     ros::spin();
 
     return 0;
