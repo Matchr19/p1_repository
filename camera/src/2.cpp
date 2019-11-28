62f9d3987742dd7581fd120f704b1d20a415a6b5
Jacob Knudsen <jknuds19@student.aau.dk>
2019-11-28 14:51:43 +0100

    Vi prøver igen

--- camera/src/2.cpp
+++ camera/src/2.cpp
@@ -1,66 +1,53 @@
-#include <ros/ros.h>
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
+/*
+  * OpenCV Example using ROS and CPP
+  */
 
-static const std::string OPENCV_WINDOW = "Image window";
+ // Include the ROS library
+ #include <ros/ros.h>
 
-class ImageConverter
-{
-    ros::NodeHandle nh_;
-    image_transport::ImageTransport it_;
-    image_transport::Subscriber image_sub_;
-    image_transport::Publisher image_pub_;
+ // Include opencv2
+ #include <opencv2/imgproc/imgproc.hpp>
+ #include <opencv2/highgui/highgui.hpp>
 
-public:
-    ImageConverter()
-        : it_(nh_)
-    {
-        // Subscrive to input video feed and publish output video feed
-        image_sub_ = it_.subscribe("/camera/image_raw", 1,
-                                   &ImageConverter::imageCb, this);
-        image_pub_ = it_.advertise("/image_converter/output_video", 1);
+ // Include CvBridge, Image Transport, Image msg
+ #include <image_transport/image_transport.h>
+ #include <cv_bridge/cv_bridge.h>
+ #include <sensor_msgs/image_encodings.h>
 
-        cv::namedWindow(OPENCV_WINDOW);
-    }
+ // OpenCV Window Name
+ static const std::string OPENCV_WINDOW = "Image window";
 
-    ~ImageConverter()
-    {
-        cv::destroyWindow(OPENCV_WINDOW);
-    }
+ // Topics
+ static const std::string IMAGE_TOPIC = "/camera/rgb/image_raw";
+ static const std::string PUBLISH_TOPIC = "/image_converter/output_video";
 
-    void imageCb(const sensor_msgs::ImageConstPtr &msg)
-    {
-        cv_bridge::CvImagePtr cv_ptr;
-        try
-        {
-            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-        }
-        catch (cv_bridge::Exception &e)
-        {
-            ROS_ERROR("cv_bridge exception: %s", e.what());
-            return;
-        }
+ // Publisher
+ ros::Publisher pub;
 
-        // Draw an example circle on the video stream
-        if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-            cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255, 0, 0));
+ void image_cb(const sensor_msgs::ImageConstPtr& msg)
+ {
+   std_msgs::Header msg_header = msg->header;
+   std::string frame_id = msg_header.frame_id.c_str();
+   ROS_INFO_STREAM("New Image from " << frame_id);
 
-        // Update GUI Window
-        cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-        cv::waitKey(3);
+   cv_bridge::CvImagePtr cv_ptr;
+   try
+   {
+     cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+   }
+   catch (cv_bridge::Exception& e)
+   {
+     ROS_ERROR("cv_bridge exception: %s", e.what());
+     return;
+   }
 
-        // Output modified video stream
-        image_pub_.publish(cv_ptr->toImageMsg());
-    }
-};
+   // Draw an example crosshair
+   //cv::drawMarker(cv_ptr->image, cv::Point(cv_ptr->image.cols/2, cv_ptr->image.rows/2),  cv::Scalar(0, 0, 255), cv::MARKER_CROSS, 10, 1);
 
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "image_converter");
-    ImageConverter ic;
-    ros::spin();
-    return 0;
-}
+   // Update GUI Window
+   cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+   cv::waitKey(3);
+
+   // Output modified video stream
+   pub.publish(cv_ptr->toImageMsg());
+ }
