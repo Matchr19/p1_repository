06113ec3b5b9a32d7e2ebaa73bb8e46dcf9ec7be
Jacob Knudsen <jknuds19@student.aau.dk>
2019-12-03 11:06:01 +0100

    Change

--- Linje/src/Hard.cpp
+++ Linje/src/Hard.cpp
@@ -11,7 +11,7 @@ geometry_msgs::Twist msg;
 void drive1(int rutenummer)
 {
     ros::Rate loop_rate(0.4);
-    for (int i{0}; i < sizeof(rute); i++)
+    for (int i{0}; i < sizeof(rute)/4; i++)
     {
         switch(rute[i])
         {
@@ -20,29 +20,31 @@ void drive1(int rutenummer)
         {
             msg.linear.x = 2;
             msg.angular.z = 0;
+            break;
         }
         case 2:
         {
 
             msg.linear.x = 0;
             msg.angular.z = 2;
+            break;
         }
         case 3:
         {
             msg.linear.x = 0;
             msg.angular.z = -2; 
+            break;
         }
         }
     drive_pub.publish(msg);
-    loop_rate.sleep();
-    
     std::cout << "Publishing " << msg.linear.x << " & " << msg.angular.z << std::endl;    
+    loop_rate.sleep();
     }
 }
 
 int main(int argc, char *argv[])
 {
-
+    std::cout << sizeof(rute);  
     ros::init(argc, argv, "Hard");
     ros::NodeHandle n;
     drive_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1);
