6a6a68f4dbc3299aff01d71ff4925c960a6b185d
Jacob Knudsen <jknuds19@student.aau.dk>
2019-12-03 12:05:02 +0100

    arg

--- line_follower_turtlebot/Linefollower/src/detect.cpp
+++ line_follower_turtlebot/Linefollower/src/detect.cpp
@@ -29,7 +29,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "ros/ros.h"
 #include "ros/console.h"
 #include "linedetect.hpp"
-#include "line_follower_turtlebot/pos.h"
+#include "line_follower_turtlebot/Linefollower/msg/pos.h"
 
 /**
  *@brief Main function that reads image from the turtlebot and provides direction to move using image processing
